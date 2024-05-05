b6c2ca882882b6c30f2f4c54952f7210d5747c9d
fa24002 <fa24002@ues.edu.sv>
2024-05-05 13:13:08 -0400

    My second commit

---
+++ demo-fa24002/src/main/java/com/iis/fa24002/demo_fa24002/App.java
@@ -0,0 +1,18 @@
+package com.iis.fa24002.demo_fa24002;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public String Hello() {
+		return "Hello World";
+	}
+	
+    public static void main( String[] args )
+    {
+    	App hw = new App();
+        System.out.println(hw.Hello() );
+    }
+}
